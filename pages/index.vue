c926d30633b3203d39499d72f5ccaf8ecc3352f0
Pponk601 <poramink601@gmail.com>
2023-03-01 15:23:06 +0700

    tailwind-starter

---
+++ pages/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="container mx-auto px-4">
+    <h1 class="text-3xl font-bold underline">
+    Hello world!
+  </h1>
+
+  </div>
+</template>
+
+<script>
+export default {
+}
+</script>
